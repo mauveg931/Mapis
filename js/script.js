a5061f8499356e62dd3e79997fd51084afd7196b
mauveg931 <maurovegamarcos@gmail.com>
2025-05-20 16:24:26 +0200

    avanzamos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 function initMap() {
-let map = new google.maps.Map(document.getElementById("map"), {
-center: { lat: 40, lng: -4 },
-zoom: 6.5,
-});
+    let map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 40, lng: -4 },
+        zoom: 6.5,
+    });
+
+    let latLng = new google.maps.LatLng(42.61777, -5.61969);
+    let marker = new google.maps.Marker({
+        position: latLng,
+        title: 'IES Villabalter',
+        map: map,
+    });
 }
 initMap();
+
+
